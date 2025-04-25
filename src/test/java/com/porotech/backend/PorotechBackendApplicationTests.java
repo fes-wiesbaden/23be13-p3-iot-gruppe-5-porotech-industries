75d66dd18444bda4701d6fdad21292b4f33a0ddc
Sleither Reyes <sleither.reyes@gmx.de>
2025-04-25 17:21:17 +0200

    init: initial base setup

---
+++ src/test/java/com/porotech/backend/PorotechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porotech.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorotechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
