4331866b9f33ea5f07648f8398e7cc2ba066ee79
Sleither Reyes <sleither.reyes@gmx.de>
2025-04-30 08:04:59 +0200

    feat: add missing dependencies for OpenCV

--- src/test/java/com/porotech/backend/PorotechBackendApplicationTests.java
+++ src/test/java/com/porotech/backend/PorotechBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.porotech.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PorotechBackendApplicationTests {
 
 	@Test
