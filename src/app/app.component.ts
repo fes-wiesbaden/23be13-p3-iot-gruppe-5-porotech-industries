7e19b7525c64a6df24228db3f540ec5952ce7735
Emil Ruckriegl <emil.ruckriegl@sinc.de>
2025-06-17 07:37:01 +0200

    styled toolbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule, 
+    MatSidenavModule, 
+    MatToolbarModule, 
+    RouterOutlet, 
+    MatIconModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+  <mat-toolbar class="mat-elevation-z3">
+    <button mat-icon-button>
+      <mat-icon>menu</mat-icon>
+    </button>
+  </mat-toolbar>
+  <mat-sidenav-container>
+    <mat-sidenav>
 
-    <router-outlet />
+    </mat-sidenav>
+    <mat-sidenav-content class="content">
+      <router-outlet></router-outlet>
+    </mat-sidenav-content>
+  </mat-sidenav-container>
+  
   `,
-  styles: [],
+  styles: [`
+    .content {
+      padding: 24px,
+    }
+   `],
 })
 export class AppComponent {
   title = 'porotech';
